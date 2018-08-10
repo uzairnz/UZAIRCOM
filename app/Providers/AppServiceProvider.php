bfd4bda11b9f4d55f3ce35ece5da5f0c3d2986ab
UZAIR MAJEED <uzairmajeed33@outlook.com>
2018-08-11 02:48:39 +0500

    :)

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,6 +4,7 @@ namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
 use Laravel\Cashier\Cashier;
+use Illuminate\Support\Facades\Schema;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -15,6 +16,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Cashier::useCurrency(config('cart.currency'), config('cart.currency_symbol'));
+        Schema::defaultStringLength(191);
     }
 
     /**
