8669c6e79aa18d5ec5b32f31c01260d89d9063f0
UZAIR MAJEED <uzairmajeed33@outlook.com>
2018-10-17 18:06:31 +0500

    Home slider blade updated

--- resources/views/layouts/front/home-slider.blade.php
+++ resources/views/layouts/front/home-slider.blade.php
@@ -4,9 +4,8 @@
             <div class="col-md-8">
                 <div class="hero-content">
                     <h1 class="hero-title">{{config('app.name')}} Holiday <br> Collection</h1>
-                    <p class="hero-text">Download this amazing e-commerce web app for <strong class="text-success">FREE!</strong></p>
-                    <a class="btn btn-success" href="https://github.com/jsdecena/laracom" target="_blank" role="button">DOWNLOAD <i class="fa fa-cloud-download"></i></a>
-                    <a class="btn btn-danger" href="https://m.do.co/c/bce94237de96" target="_blank" role="button">Build on DigitalOcean <i class="fa fa-paper-plane"></i></a>
+                    <p class="text-success">My Amazing e-commerce web app</p>
+                    <a class="btn btn-success" href="https://github.com/uzairnz/laracom" target="_blank" role="button">DOWNLOAD <i class="fa fa-cloud-download"></i></a>
                 </div>
             </div>
             <div class="col-md-4"></div>
